0c43761bcefeaf885d67bf80d03c6df09bf75447
trevorsuper <129561006+trevorsuper@users.noreply.github.com>
2025-06-22 00:53:03 -0400

    Revert to net8

--- api/webapi/Program.cs
+++ api/webapi/Program.cs
@@ -7,10 +7,12 @@ using BCrypt.Net;
 
 string password_hash = BCrypt.Net.BCrypt.HashPassword("my password");
 if (BCrypt.Net.BCrypt.Verify("my password", password_hash)) {
-    Console.WriteLine("\n\nPassword is correct.\n\n");
+    Console.WriteLine("\n\nPassword is correct.");
+    Console.WriteLine(password_hash);
+    Console.WriteLine("\n\n");
 }
 else {
-    Console.WriteLine("\n\nNo Hash.\n\n");
+    Console.WriteLine("\n\nNo Hash\n\n");
 }
 
 var builder = WebApplication.CreateBuilder(args);
